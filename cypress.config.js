20d1a1670f44e353d03be145a68f022a641d0240
Sheraz Abbas <sherazshahbukhari12@gmail.com>
2024-03-28 15:02:26 +0500

    reporting

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,29 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+/*const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});*/
