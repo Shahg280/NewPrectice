37cb168bc0d932ee849a7e95ebc06d8abd567f32
Sheraz Abbas <sherazshahbukhari12@gmail.com>
2024-03-29 11:45:32 +0500

    updatedConfig

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,17 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+     require('cypress-mochawesome-reporter/plugin')(on)
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
     },
   },
-  reporter: 'cypress-multi-reporters',
-  reporterOptions: {
-    configFile: 'reporter-config.json'
-  }
+  reporter:'cypress-mochawesome-reporter'
+  // reporter: 'cypress-multi-reporters',
+  // reporterOptions: {
+  //   configFile: 'reporter-config.json'
+  // }
 });
 
 
