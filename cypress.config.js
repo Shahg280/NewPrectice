1e6fe62ece7d244517c2756499e23e4619cb6e19
Sheraz Abbas <sherazshahbukhari12@gmail.com>
2024-04-01 16:42:49 +0500

    CircleCItest

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'mcd9a5',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -10,7 +11,8 @@ module.exports = defineConfig({
   return config;
     },
   },
-  reporter:'cypress-mochawesome-reporter'
+  reporter:'cypress-mochawesome-reporter',
+  "screenshotsFolder": "cypress/reports/html/assets"
   // reporter: 'cypress-multi-reporters',
   // reporterOptions: {
   //   configFile: 'reporter-config.json'
